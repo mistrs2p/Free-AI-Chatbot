7a14ec2c108557635628da817cc444513c72b691
Mahdi <mahdimousavi40@gmail.com>
2026-08-07 19:27:40 +0100

    feat: implement AI response generation in POST endpoint and update dependencies

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,36 @@
 import { NextResponse } from "next/server";
 
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateText } from "ai";
+
+const openrouter = createOpenAI({
+  apiKey: process.env.OPEN_ROUTER_API_KEY!,
+  baseURL: "https://openrouter.ai/api/v1",
+});
+
 export async function POST(request: Request) {
-  const body = await request.json();
-  return NextResponse.json({
-    message: `You said: "${body.content}" (AI will reply later)`,
-  });
+  try {
+    const body = await request.json();
+
+    const { text } = await generateText({
+      model: openrouter("google/gemini-2.5-flash"),
+      prompt: body.content,
+      maxOutputTokens: 2000,
+    });
+
+    return NextResponse.json({
+      message: text,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      {
+        error: "Failed to generate response.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
 }
